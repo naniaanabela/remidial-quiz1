87e8aef8d6a0cc7f03efd7b0011f22beec3664c1
Nurcahya Nania A <nania.anabela22@gmail.com>
2018-11-04 15:46:41 +0700

    membuat class Main

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author NurcahyaNaniaA
+ */
+public class Main {
+    public static void main(String[] args) {
+    Peminjam p = new Peminjam("123","Nurcahya Nania");
+        Pegawai pe = new Pegawai("111","Helmi Nur");
+        Sewa waktu = new Sewa(3);
+        Barang barang = new Barang("Kamera SLR",100000,p  , pe, waktu);
+        System.out.println(barang.info());
+        System.out.println("----------------------------");
+    }
+}
